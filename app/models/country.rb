cf3234398d8f2ed6230c90ecc308db0b53f1d174
Hoon Lee <yl.hoony@gmail.com>
2018-01-13 00:29:29 -0800

    added slug method

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,11 @@
 class Country < ActiveRecord::Base
 
+  def slug
+    self.name.gsub(/[\W\s+]/,"-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find {|country| slug.eql?(country.slug)}
+  end
+
 end
