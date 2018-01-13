a1b6e64d3b6c6a15c42888afe71015f1d0b069d8
Hoon Lee <yl.hoony@gmail.com>
2018-01-12 18:01:55 -0800

    enabled session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "this,is,session,secret"
+
+    set :public_folder, "public"
     set :views, "app/views"
   end
 
