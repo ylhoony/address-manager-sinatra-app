427148813f95547845ff22c3719158c41b7f52f2
Hoon Lee <yl.hoony@gmail.com>
2018-01-13 01:06:27 -0800

    updated logic to post countries route

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -9,7 +9,12 @@ class CountriesController < ApplicationController
   end
 
   post '/countries' do
-
+    if has_empty_value?(params) || find_by_params(params)
+      redirect "/countries/new"
+    else
+      @country = Country.create(params)
+      redirect "/countries"
+    end
   end
 
   get '/countries/:slug/edit' do
@@ -18,6 +23,14 @@ class CountriesController < ApplicationController
 
   post '/countries/slug' do
 
+  end
+
+  helpers do
+    def find_by_params(params)
+      params.find do |key, value|
+        Country.find_by("#{key.to_s}": value) # Country.find_by(key.to_sym => value)
+      end
+    end
   end
 
 end
