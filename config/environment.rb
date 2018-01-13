8bb3ea8f4fc0708e4afb0b53fe5abbabcbc7db80
Hoon Lee <yl.hoony@gmail.com>
2018-01-12 16:46:38 -0800

    set up environment in Rakefile

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,9 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, :development)
 
-ActiveRecord::Base.connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV["SINATRA"]}.sqlite"
-)
+# ActiveRecord::Base.connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV["SINATRA"]}.sqlite"
+# )
 
 require_all 'app'
