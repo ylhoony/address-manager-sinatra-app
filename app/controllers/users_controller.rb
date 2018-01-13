c4eca7f781793b5a098e41ebc802a6329d7111b1
Hoon Lee <yl.hoony@gmail.com>
2018-01-12 16:57:53 -0800

    created users controller file

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  get '/signup' do
+
+  end
+
+  post '/signup' do
+
+  end
+
+  get '/login' do
+
+  end
+
+  post '/login' do
+
+  end
+
+  get '/users' do
+
+  end
+end
