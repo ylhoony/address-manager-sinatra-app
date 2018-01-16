4fa9a061a5592bf91f1cf7f0ca282e5ae122e848
Hoon Lee <yl.hoony@gmail.com>
2018-01-16 01:15:24 -0800

    added menu to countries pages, and added test code to set is_owner value

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,6 +3,7 @@ class CompaniesController < ApplicationController
   get '/companies' do
     if logged_in?
       @current_user = current_user
+      # binding.pry
       erb :"companies/index"
     else
       redirect "/login"
@@ -22,7 +23,9 @@ class CompaniesController < ApplicationController
       @company = Company.create(params)
       @company.country = Country.find_by(name: params[:country_id])
       @company.users << current_user
+      # @company.user_companies.find_by(user_id: current_user.id).is_owner = true
       @company.save
+      # binding.pry
       redirect "companies/#{@company.id}"
     else
       redirect "/login"
