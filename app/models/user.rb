a82b230fca778cf9dafe140fcc27d92e12c9fb30
Hoon Lee <yl.hoony@gmail.com>
2018-01-16 16:03:48 -0800

    added validation to user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ActiveRecord::Base
   has_many :user_companies
   has_many :companies, through: :user_companies
 
-  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
